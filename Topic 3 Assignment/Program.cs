c1405e0a2b9700d182034f997d2f12936af294c3
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-10 14:38:39 -0500

    Parts 1-3 Completed. Part 4 is the only thing left of the topic 3 assignment.

--- Topic 3 Assignment/Program.cs
+++ Topic 3 Assignment/Program.cs
@@ -6,13 +6,15 @@
         {
             String courseName, FullName;
             int roomNumber, gradYear;
-            double price, radius;
+            double price, radius, pie, area;
 
             roomNumber = 29;
             gradYear = 2026;
             courseName = "Computer Science";
             price = 2.99;
-            radius = 3;  //Centimeters
+            pie = 3.14;  //Number to calulate radius
+            radius = 5;  //Centimeters
+            area = 3.14 * (radius * radius);  //Place holder number, Centimeters
             FullName = "Linkin Krysa";
 
             Console.WriteLine("This is room #" + roomNumber);
@@ -20,7 +22,9 @@
             Console.WriteLine("I am learning a bit about " + courseName);
             Console.WriteLine();
             Console.WriteLine("My name is " + FullName + " and I'll graduate in " + gradYear);
-
+            Console.WriteLine();
+            Console.WriteLine("To find the area of a circle," + " You will want to multiply " + pie + " by " + radius + " and radius is multiplied by itself then multiplied to pie " + "and that will give you the area.");
+            Console.WriteLine("When we multiply " + pie + " by " + radius + ", " + "You will get " + area);
 
         }
     }
