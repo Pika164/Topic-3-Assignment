a05aad3815b93cd027e2eee545fa04826d7217d9
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-10 14:02:06 -0500

    Add project files.

---
+++ Topic 3 Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Topic_3_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String roomnumber, CourseName;
+            double price;
+
+            roomnumber = 29;
+            CourseName = "Computer Science";
+            price = 2.99;
+        }
+    }
+}
