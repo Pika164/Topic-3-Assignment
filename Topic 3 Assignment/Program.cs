75f7b944f4106cae53103281e9c250b974fbf3e4
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-11 13:45:36 -0500

    Finished all of the parts for the assignment.

--- Topic 3 Assignment/Program.cs
+++ Topic 3 Assignment/Program.cs
@@ -1,15 +1,20 @@
-﻿namespace Topic_3_Assignment
+﻿using Microsoft.VisualBasic;
+
+namespace Topic_3_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             String courseName, FullName;
-            int roomNumber, gradYear;
+            int roomNumber, gradYear, length, width, perimeter;
             double price, radius, pie, area;
 
             roomNumber = 29;
             gradYear = 2026;
+            length = 6;
+            width = 11;
+            perimeter = (2 * (length + width));
             courseName = "Computer Science";
             price = 2.99;
             pie = 3.14;  //Number to calulate radius
@@ -25,6 +30,8 @@
             Console.WriteLine();
             Console.WriteLine("To find the area of a circle," + " You will want to multiply " + pie + " by " + radius + " and radius is multiplied by itself then multiplied to pie " + "and that will give you the area.");
             Console.WriteLine("When we multiply " + pie + " by " + radius + ", " + "You will get " + area);
+            Console.WriteLine();
+            Console.WriteLine("My favourite mathmatical formula is finding the perimeter of a rectangle. " + "If we take " + length + " and multiply it by " + width + " We get " + perimeter);
 
         }
     }
